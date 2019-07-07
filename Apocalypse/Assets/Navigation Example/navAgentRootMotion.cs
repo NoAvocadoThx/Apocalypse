feb1d43505dc7352d26b1a86b207fc653ba27f79
noavocadothx <wildpepsi0808@gmail.com>
2019-07-06 19:42:27 -0700

    navigation with animation

---
+++ Apocalypse/Assets/Navigation Example/navAgentRootMotion.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class navAgentRootMotion : MonoBehaviour
+{
+    //inspector assigned vars
+    public AIWaypointNetwork WaypointNetwork = null;
+    public int curIndex = 0;
+    public bool hasPath = false;
+    public bool pathPending = false;
+    public bool pathStale = false;
+    public NavMeshPathStatus pathStatus = NavMeshPathStatus.PathInvalid;
+    public AnimationCurve jumpCurve = new AnimationCurve();
+    public bool MixedMode = true;
+
+    private NavMeshAgent _navAgent = null;
+    private Animator _animator = null;
+    
+    private float _smoothAngle = 0.0f;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //NavMeshAgent referrence
+        _navAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+       
+        //only NavMeshAgent moving but model not moving
+
+        //_navAgent.updatePosition = false;
+         _navAgent.updateRotation = false;
+
+        if (WaypointNetwork == null) return;
+        //move agent to destination (hard coded location)
+        /* if (WaypointNetwork.waypoints[curIndex])
+         {
+             _navAgent.destination = WaypointNetwork.waypoints[curIndex].position;
+         }*/
+
+
+
+        //move agent to desination (automatic)
+        setNextDestination(false);
+
+    }
+
+    void setNextDestination (bool increment)
+    {
+
+        //no network then return
+        if (!WaypointNetwork) return;
+        int incStep = increment ? 1 : 0;
+        Transform nextWaypointTransform = null;
+
+
+        //if next waypoint reachable +1 or go to 0
+        // Calculate index of next waypoint factoring in the increment with wrap-around and fetch waypoint 
+        int nextWaypoint = (curIndex + incStep >= WaypointNetwork.waypoints.Count)?0:curIndex+incStep;
+        nextWaypointTransform = WaypointNetwork.waypoints[nextWaypoint];
+        if (nextWaypointTransform)
+        {
+            //update 
+            curIndex = nextWaypoint;
+            _navAgent.destination = nextWaypointTransform.position;
+            return;
+        }
+            
+        
+       curIndex++;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+       // int turnOnSpot;
+
+        hasPath = _navAgent.hasPath;
+        pathPending = _navAgent.pathPending;
+        pathStale = _navAgent.isPathStale;
+        pathStatus = _navAgent.pathStatus;
+
+
+        // Transform agents desired velocity into local space
+        Vector3 localDesiredVelocity = transform.InverseTransformVector(_navAgent.desiredVelocity);
+
+        // Get angle in degrees we need to turn to reach the desired velocity direction
+        float angle = Mathf.Atan2(localDesiredVelocity.x, localDesiredVelocity.z) * Mathf.Rad2Deg;
+
+        // Smoothly interpolate towards the new angle
+        _smoothAngle = Mathf.MoveTowardsAngle(_smoothAngle, angle, 80.0f * Time.deltaTime);
+
+        // Speed is simply the amount of desired velocity projected onto our own forward vector
+        float speed = localDesiredVelocity.z;
+
+        // Set animator parameters
+        _animator.SetFloat("Angle", _smoothAngle);
+        _animator.SetFloat("Speed", speed, 0.1f, Time.deltaTime);
+
+      
+
+        /*
+        // Perform corss product on forard vector and desired velocity vector. If both inputs are Unit length
+        // the resulting vector's magnitude will be Sin(theta) where theta is the angle between the vectors.
+        Vector3 cross = Vector3.Cross(transform.forward, _navAgent.desiredVelocity.normalized);
+
+        // If y component is negative it is a negative rotation else a positive rotation
+        float horizontal = (cross.y < 0) ? -cross.magnitude : cross.magnitude;
+        // Scale into the 2.32 range for our animator
+        horizontal = Mathf.Clamp(horizontal * 2.32f, -2.32f, 2.32f);
+        */
+
+        if (_navAgent.desiredVelocity.sqrMagnitude > Mathf.Epsilon)
+        {
+            if (!MixedMode ||
+                (MixedMode && Mathf.Abs(angle) < 80.0f && _animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Locomotion")))
+            {
+                Quaternion lookRotation = Quaternion.LookRotation(_navAgent.desiredVelocity, Vector3.up);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 5.0f * Time.deltaTime);
+            }
+        }
+
+        /*
+        //control turning offsets, - means left turn + means right turn
+        _animator.SetFloat("Horizontal", horizontal, 0.1f, Time.deltaTime);
+        //control animator speed
+        _animator.SetFloat("Vertical", _navAgent.desiredVelocity.magnitude, 0.1f, Time.deltaTime);
+
+        _animator.SetInteger("TurnOnSpot", turnOnSpot);*/
+        //if agent is on offMeshLink, do "jump" for 2 seconds and then return back to current bahavior
+       /* if (_navAgent.isOnOffMeshLink)
+        {
+            StartCoroutine(Jump(1.0f));
+            return;
+        }*/
+
+        //if the agent has path and it is in a path
+        //if the next waypoint is completely not reachable or partially reachable, skip next waypoint and go to next one
+        if (!hasPath && !pathPending||pathStatus==NavMeshPathStatus.PathInvalid/*||pathStatus==NavMeshPathStatus.PathPartial*/)
+        {
+            setNextDestination(true);
+        }
+        else
+        {
+            //if next path destination changes to a unreachable or cost-increment..
+            //regenerate new path
+            if (pathStale)
+            {
+                setNextDestination(false);
+            }
+        }
+    }
+
+    void OnAnimatorMove()
+    {
+        // If we are in mixed mode and we are not in the Locomotion state then apply rooy rotation
+        if (MixedMode && !_animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Locomotion"))
+            transform.rotation = _animator.rootRotation;
+
+        // Override Agent's velocity with the velocity of the root motion
+        _navAgent.velocity = _animator.deltaPosition / Time.deltaTime;
+    }
+
+
+    //jump method
+    IEnumerator Jump(float duration)
+    {
+        OffMeshLinkData data = _navAgent.currentOffMeshLinkData;
+        // Start Position is agent current position
+        Vector3 startPos = _navAgent.transform.position;
+        // End position is fetched from OffMeshLink data and adjusted for baseoffset of agent
+        Vector3 endPos = data.endPos + (_navAgent.baseOffset * Vector3.up);
+        float time = 0.0f;
+        while (time <= duration)
+        {
+            float t = time / duration;
+            //interplote between startPos and endPos plus the height offset of the animation curve
+            _navAgent.transform.position = Vector3.Lerp(startPos, endPos, t)+(jumpCurve.Evaluate(t)*Vector3.up);
+            time += Time.deltaTime;
+            // Accumulate time and yield each frame
+            yield return null;
+        }
+        _navAgent.CompleteOffMeshLink();
+    }
+
+
+}
