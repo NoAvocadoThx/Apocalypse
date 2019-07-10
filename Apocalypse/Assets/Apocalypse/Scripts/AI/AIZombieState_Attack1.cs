043b4e410084d4c9a84552ba570612ba30098949
noavocadothx <wildpepsi0808@gmail.com>
2019-07-10 02:15:42 -0700

    attack state WIP

---
+++ Apocalypse/Assets/Apocalypse/Scripts/AI/AIZombieState_Attack1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIZombieState_Attack1 : AIZombieState
+{
+    // Inspector Assigned
+    [SerializeField] [Range(0, 10)] float _speed = 0.0f;
+    [SerializeField] [Range(0.0f, 1.0f)] float _lookAtWeight = 0.7f;
+    [SerializeField] [Range(0.0f, 90.0f)] float _lookAtAngleThreshold = 15.0f;
+    [SerializeField] float _slerpSpeed = 5.0f;
+}
